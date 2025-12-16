01d20e9317ce3e811c14cdd7a45cbea1a30ad2da
{ppbotelhoDev} <{ppbotelho08@gmail.com}>
2025-12-16 14:13:15 -0300

    feat(ShowCase): Conclusão da seção Experiência Mobile com mockups e destaque visual.

--- src/components/layout/ShowCase.jsx
+++ src/components/layout/ShowCase.jsx
@@ -7,15 +7,18 @@ import perfil from "../../assets/mockup-perfil.png";
 
 const ShowCase = () => {
   const baseMockupWrapper =
-    "shadow-2xl transition duration-500 hover:scale-[1.08]";
+    "transition duration-500 hover:scale-[1.08] space-y-5 font-semibold";
   const highlightMockupWrapper =
-    "shadow-2xl md:scale-[0.9] md:z-10 relative transition duration-500 md:hover:scale-[0.95]";
+    "md:scale-[0.9] md:z-10 relative transition duration-500 md:hover:scale-[0.95] space-y-5 font-semibold";
+  const textSubtitle = "text-gray-200 text-[14px]"
 
   return (
     <section className="bg-slate-950 py-20 ">
       <div className="max-w-7xl mx-auto px-4 text-center ">
         <h2 className="text-white text-3xl md:text-4xl font-extrabold mb-4 ">
-          <span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-violet-600 to-cyan-500">Experiência Mobile</span>
+          <span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-violet-600 to-cyan-500">
+            Experiência Mobile
+          </span>
         </h2>
         <h3 className=" text-gray-200 text-xl md:text-xl max-w-2xl mx-auto mb-1 md:mb-10">
           Seus convidados acessam pelo celular e compartilham momentos em tempo
@@ -27,8 +30,9 @@ const ShowCase = () => {
               <img
                 src={feed}
                 alt="Mockup da Timeline do App Memora"
-                className="w-full h-auto rounded-3xl"
+                className="w-full h-auto rounded-3xl shadow-[0_0_10px_3px_rgba(6,182,212,0.2)]"
               />
+              <h1 className={textSubtitle}>Feed</h1>
             </div>
           </div>
           <div className="w-full md:w-70">
@@ -36,8 +40,9 @@ const ShowCase = () => {
               <img
                 src={camera}
                 alt="Mockup da Câmera do App Memora"
-                className="w-full h-auto rounded-3xl"
+                className="w-full h-auto rounded-3xl shadow-[0_0_10px_3px_rgba(6,182,212,0.2)]"
               />
+              <h1 className={textSubtitle}>Capture o momento</h1>
             </div>
           </div>
           <div className="w-full md:w-60">
@@ -45,13 +50,14 @@ const ShowCase = () => {
               <img
                 src={perfil}
                 alt="Mockup do Perfil do Convidado do App Memora"
-                className="w-full h-auto rounded-3xl"
+                className="w-full h-auto rounded-3xl shadow-[0_0_10px_3px_rgba(6,182,212,0.2)]"
               />
+              <h1 className={textSubtitle}>Seu perfil</h1>
             </div>
           </div>
         </div>
 
-        <div className="mt- md:mt-10 max-w-fit mx-auto rounded-full p-[1px] md:p-[2px] bg-purple-800 shadow-[0_0_10px_0_rgba(59,130,246,0.2)]">
+        <div className="mt- md:mt-10 max-w-fit mx-auto rounded-full p-[1px] md:p-[2px] bg-purple-800 shadow-[0_0_10px_3px_rgba(6,182,212,0.2)]">
           <div className="bg-[#130E2E] rounded-full p-3 md:p-4 transition duration-300">
             <p className="text-white md:text-lg font-semibold px-4">
               ✨ A experiência social que seus convidados já dominam
